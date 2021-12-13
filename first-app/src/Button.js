6fcb2ca47a0c84029755c31ea7d605052d585936
tahrer007 <tahrer_Ad@outlook.com>
2021-12-13 16:12:55 +0200

    update

---
+++ first-app/src/Button.js
@@ -0,0 +1,12 @@
+
+
+const Button = ()=>{
+
+    return (
+    <button style={{fontWeight:"800"}} >important </button>
+    
+     )
+     ;
+
+}
+export default Button
---
+++ first-app/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from './Button';
+
+
+const App =() =>{
+  return ( <Button/> );};
+
+ReactDOM.render(<App />,document.querySelector('#root'))
