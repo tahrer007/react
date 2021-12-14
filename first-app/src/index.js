73ce5101712f59333ab03360ed646cb85739772d
tahrer007 <tahrer_Ad@outlook.com>
2021-12-14 21:10:22 +0200

    1

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from './Button';
+import Button from "./Button";
 
-
-const App =() =>{
-  return ( 
-    <div className="App">
-      hiiiii 
-      <div> hiii second time </div>
-      <Button text="important" weight="900"/>
-      <Button text="not important"  weight="normal"/>
- 
-
-    </div>
+const App = () => {
   
- // <Button text= "not important" /> 
-    
-    
-    );};
+   
+      window.navigator.geolocation.getCurrentPosition(
+         (position) =>
+      console.log(position),
+      (err) => console.log(err))
 
-ReactDOM.render(<App />,document.querySelector('#root'))
+
+    return <Button text="important" weight="900"/>
+
+    //<Button text= "not important" />
+    //<Button text="important" weight="900"/>
+    //<Button text="not important"  weight="normal"/>
+ 
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
